8a8d0dc4c671bd18a3d08bfec3a4397ca19d8754
fegrdz <fegrdz@gmail.com>
2024-05-19 17:43:49 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+const slides = document.querySelectorAll('.slide');
+let currentSlide = 0;
+
+function nextSlide() {
+  slides[currentSlide].classList.remove('active');
+  currentSlide = (currentSlide + 1) % slides.length;
+  slides[currentSlide].classList.add('active');
+}
+
+setInterval(nextSlide, 5000); // Cambia de diapositiva cada 5 segundos
